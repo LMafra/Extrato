193a2d6d50d9b20a9c5b5f90555a3dbf3b610ddd
Andre Giampaolo <andregiampaolo@gmail.com>
2015-11-07 03:35:36 -0200

    Corrigindo conflitos de banco de dados

--- app/models/perfomance.rb
+++ app/models/perfomance.rb
@@ -3,4 +3,5 @@ class Perfomance < ActiveRecord::Base
   belongs_to :federations
   belongs_to :role
   has_many :dimensions
+  has_many :perfomance_answers
 end
--- db/migrate/20151107051439_create_perfomance_answers.rb
+++ db/migrate/20151107051439_create_perfomance_answers.rb
@@ -2,7 +2,7 @@ class CreatePerfomanceAnswers < ActiveRecord::Migration
   def change
     create_table :perfomance_answers do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :userLogged, index: true, foreign_key: true
+      t.integer :userLogged
       t.references :perfomance, index: true, foreign_key: true
       t.references :dimension, index: true, foreign_key: true
       t.references :question, index: true, foreign_key: true
